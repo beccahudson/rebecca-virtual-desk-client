c03d577fd6e5c0d9a329812ebf36cf1d805caefb
Rhudson <contact@rebeccahudson.me>
2020-08-24 16:39:42 -0400

    fixed api issue

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000",
+  // API_ENDPOINT: "http://localhost:8000",
   // API_ENDPOINT: `https://postgresql-fitted-25153.herokuapp.com`,
-  API_KEY: process.env.REACT_APP_API_KEY,
+  API_ENDPOINT: `https://virtual-desk.herokuapp.com`,
+  TOKEN_KEY: process.env.REACT_APP_API_KEY,
 };
