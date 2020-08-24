9369f924c8c6757b87158fc23784e216a08c9f22
Rhudson <contact@rebeccahudson.me>
2020-08-24 17:00:59 -0400

    cleaned up code for predeploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
   // API_ENDPOINT: "http://localhost:8000",
-  // API_ENDPOINT: `https://postgresql-fitted-25153.herokuapp.com`,
   API_ENDPOINT: `https://virtual-desk.herokuapp.com`,
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
 };
